2ca895a30ca3553b6e7f2f2966ddf31486eefb6b
marc.lundgren <marc.lundgren@kareo.com>
2020-10-10 00:28:13 -0700

    Login with AWS Cognito

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "free-notes-app-uploads",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://av8p4ixuwj.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_jxweLhF23",
+    APP_CLIENT_ID: "2o3of812v06cb3jt942k0io9v4",
+    IDENTITY_POOL_ID: "us-east-2:77a97087-1892-40b5-af84-8aa4dcc97ca7",
+  },
+};
