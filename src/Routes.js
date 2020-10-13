1d217b90d71dc5aa061009ad30b16a1db0de6f05
marc.lundgren <marc.lundgren@kareo.com>
2020-10-12 21:08:21 -0700

    Building our React app

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
+import { Switch, Route } from "react-router-dom";
 import React from "react";
-import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
@@ -7,30 +7,31 @@ import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
 import Settings from "./containers/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/login">
+      <UnauthenticatedRoute exact path="/login">
         <Login />
-      </Route>
-      <Route exact path="/signup">
+      </UnauthenticatedRoute>
+      <UnauthenticatedRoute exact path="/signup">
         <Signup />
-      </Route>
-      <Route exact path="/notes/new">
-        <NewNote />
-      </Route>
-      <Route exact path="/notes/:id">
-        <Notes />
-      </Route>
-      <Route exact path="/settings">
+      </UnauthenticatedRoute>
+      <AuthenticatedRoute exact path="/">
+        <Home />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/settings">
         <Settings />
-      </Route>
-      {/* Finally, catch all unmatched routes */}
-      <Route>
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/new">
+        <NewNote />
+      </AuthenticatedRoute>
+      <AuthenticatedRoute exact path="/notes/:id">
+        <Notes />
+      </AuthenticatedRoute>
+      <Route path="*">
         <NotFound />
       </Route>
     </Switch>
